fed63c451e3a2d6155a44b6e4a33d0b4f696a961
mini-bryanlin <bryanlin316@gmail.com>
2025-07-26 00:31:15 -0400

    first commit

---
+++ include/chunk.h
@@ -0,0 +1,10 @@
+#ifndef bryte_chunk_h
+
+#define bryte_chunk_h
+#include "common.h"
+
+typedef enum {
+    OP_RETURN,
+} OpCode;
+
+#endif
---
+++ include/common.h
@@ -0,0 +1,7 @@
+#if !defined bryte_common_h
+#define bryte_common_h
+#include <stdbool.h>
+#include <stddef.h>
+#include <stdint.h>
+
+#endif
