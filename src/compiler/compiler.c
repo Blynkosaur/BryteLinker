cc4e07fb11058ccff7d1e34f25c98a3514eb3ce4
mini-bryanlin <bryanlin316@gmail.com>
2025-08-10 23:36:30 -0400

    more

--- src/compiler/compiler.c
+++ src/compiler/compiler.c
@@ -5,4 +5,18 @@
 
 void compile (const char* source){
     initScanner(source);
+    int line = -1;
+
+while (1){
+    Token token = scanToken();
+    if(token.line != line){
+        printf("%4d ", token.line);
+        line = token.line;
+    }else{
+        printf("   | ");
+    }
+    printf("%2d '%.s'\n", token.type, token.length, token.start);
+    ir(token.type == TOKEN_EOF) break;
 }
+}
+
