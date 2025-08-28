547be46e47f9abf21ffe44b5efc6968561c8eba0
mini-bryanlin <bryanlin316@gmail.com>
2025-08-27 21:45:59 -0400

    more shit

--- src/datastructures/hashmap.c
+++ src/datastructures/hashmap.c
@@ -32,17 +32,17 @@ void freeTable(Table *table)
     initTable(table);
 }
 static Entry DELETED_ENTRY = {.key = NULL, .value = 0};
-static Entry*lookUp(Table *table, StringObj *key)//returns the index
+static Entry *lookUp(Table *table, StringObj *key) // returns the index
 {
-    int capacity = table ->capacity;
+    int capacity = table->capacity;
     char *string_key = key->chars;
-    uint32_t hash = (key->hash)%capacity;
+    uint32_t hash = (key->hash) % capacity;
     Entry *current = (table->entries)[hash];
     while (current != NULL)
     {
         char *entry_key = current->key->chars;
 
-        if (strlen(entry_key) == strlen(key) && strcmp(key, entry_key) == 0)
+        if (strlen(entry_key) == strlen(key) && strcmp(key, entry_key) == 0 && current != &DELETED_ENTRY)
         {
             return current;
         } // compare key from the current entry to the one as a parameter
@@ -53,52 +53,59 @@ static Entry*lookUp(Table *table, StringObj *key)//returns the index
     }
     return NULL;
 }
-static int get_index(Table* table, StringObj* key){
-    uint32_t init_hash = key ->hash%(table->capacity);
+static int get_index(Table *table, StringObj *key)
+{
+    uint32_t init_hash = key->hash % (table->capacity);
     int index = init_hash;
-    Entry* current = table->entries[index];
+    Entry *current = table->entries[index];
 
-    while (current != NULL){
-        if(strlen(current->key->chars)&& strcmp(key,current->key->chars)==0){
+    while (current != NULL)
+    {
+        if (strlen(current->key->chars) && strcmp(key, current->key->chars) == 0 && current != &DELETED_ENTRY)
+        {
             return index;
         }
-        index ++;
+        index++;
     }
     return -1;
 }
-void growTable(Table* table, int new_size){
-    if (new_size < BASE_SIZE){
+void growTable(Table *table, int new_size)
+{
+    if (new_size < BASE_SIZE)
+    {
         return table;
     }
-    Table * new_table = malloc(sizeof(Table));
-    new_table -> capacity = new_size;
-    new_table -> count = 0;
-    new_table -> entries = calloc(new_size, sizeof(Entry));
-    
+    Table *new_table = malloc(sizeof(Table));
+    new_table->capacity = new_size;
+    new_table->count = 0;
+    new_table->entries = calloc(new_size, sizeof(Entry));
+
     return new_table;
 }
 bool set(Table *table, Value value, StringObj *key)
 {
-Entry* lookup = lookUp(table,key);
-if (lookup != NULL){//just changing the value that's already there 
-lookup->value = value;
-return true;
+    Entry *lookup = lookUp(table, key);
+    if (lookup != NULL)
+    { // just changing the value that's already there
+        lookup->value = value;
+        return true;
+    }
+    // TODO: EXPAND THE HASHMAP OR DECREASE ITS SIZE DYNAMICALLY I AM WAY TO TIRED FOR THIS SHIT
+    Entry *new_entry = malloc(sizeof(Entry));
+    new_entry->key = key;
+    new_entry->value = value;
+    int init_index = key->hash % (table->capacity);
+    while ((table->entries)[init_index] != NULL)
+    {
+        init_index++;
+    }
+    table->entries[init_index] = new_entry;
 }
-//TODO: EXPAND THE HASHMAP OR DECREASE ITS SIZE DYNAMICALLY I AM WAY TO TIRED FOR THIS SHIT
-Entry * new_entry = malloc(sizeof(Entry));
-new_entry->key = key;
-new_entry-> value = value;
-int init_index= key->hash%(table->capacity);
-while ((table->entries)[init_index]!= NULL){
-    init_index++;
-}
-table->entries[init_index] = new_entry;
-}
-bool delete(Table* table, StringObj * key){
-    int index = get_index(table, key );
-    Entry* temp_ptr = table-> entries[index];
+bool delete(Table *table, StringObj *key)
+{
+    int index = get_index(table, key);
+    Entry *temp_ptr = table->entries[index];
     free(temp_ptr);
-    table -> entries[index] = &(DELETED_ENTRY);
+    table->entries[index] = &(DELETED_ENTRY);
     // NOW RESIZE DOWN IF COUNT IS SMALLER THAN 10%
 }
-
