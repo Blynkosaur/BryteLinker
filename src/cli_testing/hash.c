3b8233312d3e4abe8ac53f298f6d744fb91cf503
mini-bryanlin <bryanlin316@gmail.com>
2025-09-03 00:04:39 -0400

    more debug interning

---
+++ src/cli_testing/hash.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../../include/bytecode/object.h"
+#include "../../include/bytecode/value.h"
+#include "../../include/datastructures/hashmap.h"
+
+
+int main(void){
+    while(1){
+        printf("> ");
+        
+    }
+}
