25af562a5a06ccef42543e8af44969e8e1b64df4
mini-bryanlin <bryanlin316@gmail.com>
2025-10-18 23:51:48 -0400

    setup function objects

--- include/bytecode/object.h
+++ include/bytecode/object.h
@@ -1,46 +1,57 @@
 #ifndef bryte_object_h
 
 #define bryte_object_h
-
 #include "../common.h"
+#include "./chunk.h"
 #include "./value.h"
 
-typedef enum{
-    OBJ_STRING,
-}ObjType;
+typedef enum {
+  OBJ_FUNCTION,
+  OBJ_STRING,
+} ObjType;
 
 struct Obj {
-    ObjType type;
-    Obj* next; // --> intrusive linkedlist whatever that is. Cuz we're not making another linkedlist
- };
+  ObjType type;
+  Obj *next; // --> intrusive linkedlist whatever that is. Cuz we're not making
+             // another linkedlist
+};
 
- struct StringObj{
-    Obj obj;// --> extends Obj struct so some kind of inheritance
-    /*having obj as StringObj's first field means that they are aligned
-    So basically you can cast StringObj* to Obj* and call it's attributes (e.g type)*/
-    int length;
-    char* chars;
-    uint32_t hash;
+struct StringObj {
+  Obj obj; // --> extends Obj struct so some kind of inheritance
+  /*having obj as StringObj's first field means that they are aligned
+  So basically you can cast StringObj* to Obj* and call it's attributes (e.g
+  type)*/
+  int length;
+  char *chars;
+  uint32_t hash;
+};
+typedef struct {
+  Obj obj;
+  int param_count;
+  Chunk chunk;
+  StringObj *name;
 
- };
+} FunctionObj;
 
+void printObject(Value value);
 
- void printObject(Value value);
-
-
-//reminder: OBJ_VAL has a type "obj" and the payload is a pointer the the object struct
+// reminder: OBJ_VAL has a type "obj" and the payload is a pointer the the
+// object struct
 #define OBJ_TYPE(value) (PAYLOAD_OBJ(value)->type)
 
-static inline bool isObjType(Value value, ObjType type){
-    return IS_OBJ(value) && OBJ_TYPE(value) == type;
+static inline bool isObjType(Value value, ObjType type) {
+  return IS_OBJ(value) && OBJ_TYPE(value) == type;
 }
-//isObjType is not a macro cuz value is called twice, so compiler would just copy and paste the code so if we called isObjType(pop()) 
-// pop() would be called twice, ig be careful
- #define IS_STRING(value) isObjType(value,OBJ_STRING)
-#define PAYLOAD_STRING(value) ((StringObj *)(PAYLOAD_OBJ(value))) // --> converts value's obj* to stringobj*
-#define PAYLOAD_CSTRING(value) (((StringObj*)(PAYLOAD_OBJ(value)))->chars)
-StringObj* copyString(const char* chars, int length);
+// isObjType is not a macro cuz value is called twice, so compiler would just
+// copy and paste the code so if we called isObjType(pop())
+//  pop() would be called twice, ig be careful
+#define IS_STRING(value) isObjType(value, OBJ_STRING)
+#define IS_FUNCTION(value) isObjType(value, OBJ_FUNCTION)
+#define PAYLOAD_STRING(value)                                                  \
+  ((StringObj *)(PAYLOAD_OBJ(value))) // --> converts value's obj* to stringobj*
+#define PAYLOAD_CSTRING(value) (((StringObj *)(PAYLOAD_OBJ(value)))->chars)
+StringObj *copyString(const char *chars, int length);
 
-StringObj* makeObjWithString(char* chars, int length); 
-
+StringObj *makeObjWithString(char *chars, int length);
+FunctionObj *makeFunction();
 #endif
--- src/memory.c
+++ src/memory.c
@@ -1,27 +1,33 @@
 #include "../include/memory.h"
-#include "../include/vm/vm.h"
 #include "../include/bytecode/object.h"
+#include "../include/vm/vm.h"
 #include <stdio.h>
 
-static void freeObject(Obj* object){
-    //reminder Obj --> {.type, .next}
-    //StringObj --> {.obj, .chars, .length,}
-    switch (object->type){
-        case OBJ_STRING:{
-            StringObj* string = (StringObj*)object;
-            printf("Freed string: %s\n", string->chars);
-            free(string->chars);
-            free(object);
-            break;
-        }
-    }
+static void freeObject(Obj *object) {
+  // reminder Obj --> {.type, .next}
+  // StringObj --> {.obj, .chars, .length,}
+  switch (object->type) {
+  case OBJ_STRING: {
+    StringObj *string = (StringObj *)object;
+    printf("Freed string: %s\n", string->chars);
+    free(string->chars);
+    free(object);
+    break;
+  }
+  case OBJ_FUNCION: {
+    FunctionObj *function = (FunctionObj *)object;
+    printf("Freed function: %s\n", function->name->chars);
+    freeChunk(&function->chunk);
+    free(function->name->chars);
+    free(function->name);
+  }
+  }
 }
- void freeObjects(){
-    Obj* head = vm.objectsHead;
-    while (head != NULL){
-        Obj* temp_next = head->next;
-        freeObject(head);
-        head = temp_next;
-    }
+void freeObjects() {
+  Obj *head = vm.objectsHead;
+  while (head != NULL) {
+    Obj *temp_next = head->next;
+    freeObject(head);
+    head = temp_next;
+  }
 }
-
