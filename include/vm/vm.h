0e9a0aa27257f725be535c14f53dee68cb41c51d
mini-bryanlin <bryanlin316@gmail.com>
2025-10-22 23:58:20 -0400

    callframe

--- include/vm/vm.h
+++ include/vm/vm.h
@@ -1,35 +1,39 @@
 #ifndef bryte_vm_h
 #define bryte_vm_h
-#include "../bytecode/chunk.h"
 #include "../../include/bytecode/value.h"
+#include "../bytecode/chunk.h"
 #include "../datastructures/hashmap.h"
+#include <cstdint>
 #define STACK_MAX 256
-typedef struct{
-    Chunk*chunk;
-    uint8_t* ip;
-    Value stack[STACK_MAX];
-    Value *stackTop;
-    Obj* objectsHead;
-    Table strings;// for string objects to be interned
-    Table globals;// for global variables
+typedef struct {
+  Chunk *chunk;
+  uint8_t *ip;
+  Value stack[STACK_MAX];
+  Value *stackTop;
+  Obj *objectsHead;
+  Table strings; // for string objects to be interned
+  Table globals; // for global variables
 
-}VM;
-
+} VM;
+typedef struct {
+  FunctionObj *function;
+  uint8_t ip;
+  Value *slots;
+} CallFrame;
 extern VM vm;
 
 void push(Value value);
 Value pop();
 
+typedef enum {
+  INTERPRET_OK,
+  INTERPRET_COMPILE_ERROR,
+  INTERPRET_RUNTIME_ERROR
 
-typedef enum{
-    INTERPRET_OK,
-    INTERPRET_COMPILE_ERROR,
-    INTERPRET_RUNTIME_ERROR
-
-}InterpretResult;
+} InterpretResult;
 
 void initVM();
 void freeVM();
-InterpretResult interpret(const char* source);
+InterpretResult interpret(const char *source);
 
 #endif
